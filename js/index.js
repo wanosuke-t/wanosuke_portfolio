41fffc8a904b2e694ecf2dc6dec2f389a2d5661f
和ノ助（わのすけ） <wanosuke.t@gmail.com>
2025-02-04 16:48:03 +0900

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -4,3 +4,9 @@ jQuery("#js-button-drawer").on("click", function () {
   jQuery("#js-drawer").slideToggle();
   jQuery("body").toggleClass("is-fixed");
 });
+
+jQuery(".js-button").on("click", function () {
+  jQuery("#js-button-drawer").removeClass("is-checked");
+  jQuery("#js-drawer").slideUp();
+  jQuery("body").removeClass("is-fixed");
+});
