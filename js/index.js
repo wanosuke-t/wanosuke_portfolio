42824b38bdd8914a9777b00980e3ec8192d17467
和ノ助（わのすけ） <wanosuke.t@gmail.com>
2025-02-05 18:10:02 +0900

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -1,12 +1,34 @@
+let breakpoint;
+
+function checkWindowSize() {
+  if (window.matchMedia("(max-width: 768px)").matches) {
+    breakpoint = "sp";
+    jQuery("#js-drawer").css("display", "none");
+  } else {
+    breakpoint = "pc";
+    jQuery("#js-drawer").css("display", "block");
+  }
+}
+
+// ページロード時に実行
+checkWindowSize();
+
+// ウィンドウサイズ変更時に実行
+window.addEventListener("resize", checkWindowSize);
+
 // ハンバーガーボタンとドロワー
 jQuery("#js-button-drawer").on("click", function () {
-  jQuery(this).toggleClass("is-checked");
-  jQuery("#js-drawer").slideToggle();
-  jQuery("body").toggleClass("is-fixed");
+  if (breakpoint === "sp") {
+    jQuery(this).toggleClass("is-checked");
+    jQuery("#js-drawer").slideToggle();
+    jQuery("body").toggleClass("is-fixed");
+  }
 });
 
 jQuery(".js-button").on("click", function () {
-  jQuery("#js-button-drawer").removeClass("is-checked");
-  jQuery("#js-drawer").slideUp();
-  jQuery("body").removeClass("is-fixed");
+  if (breakpoint === "sp") {
+    jQuery("#js-button-drawer").removeClass("is-checked");
+    jQuery("#js-drawer").slideUp();
+    jQuery("body").removeClass("is-fixed");
+  }
 });
